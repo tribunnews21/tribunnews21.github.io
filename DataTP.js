20945eeb947985737331ffffb09f8997eec62b21
tribunnews21 <82363416+tribunnews21@users.noreply.github.com>
2021-04-13 01:36:39 +0700

    Create DataTP.js

---
+++ DataTP.js
@@ -0,0 +1,353 @@
+var ramadanDate = 1;
+
+        const jadwal = [{
+            "tanggal": "Selasa, 13\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:51",
+            "dhuha": "06:18",
+            "dzuhur": "11:57",
+            "ashar": "15:14",
+            "maghrib": "17:56",
+            "isya": "19:05",
+            "date": 1
+        }, {
+            "tanggal": "Rabu, 14\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:51",
+            "dhuha": "06:18",
+            "dzuhur": "11:56",
+            "ashar": "15:14",
+            "maghrib": "17:55",
+            "isya": "19:05",
+            "date": 2
+        }, {
+            "tanggal": "Kamis, 15\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:56",
+            "ashar": "15:14",
+            "maghrib": "17:55",
+            "isya": "19:04",
+            "date": 3
+        }, {
+            "tanggal": "Jumat, 16\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:56",
+            "ashar": "15:14",
+            "maghrib": "17:55",
+            "isya": "19:04",
+            "date": 4
+        }, {
+            "tanggal": "Sabtu, 17\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:56",
+            "ashar": "15:14",
+            "maghrib": "17:54",
+            "isya": "19:04",
+            "date": 5
+        }, {
+            "tanggal": "Minggu, 18\/04\/2021",
+            "imsak": "04:28",
+            "subuh": "04:38",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:56",
+            "ashar": "15:14",
+            "maghrib": "17:54",
+            "isya": "19:03",
+            "date": 6
+        }, {
+            "tanggal": "Senin, 19\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:55",
+            "ashar": "15:14",
+            "maghrib": "17:53",
+            "isya": "19:03",
+            "date": 7
+        }, {
+            "tanggal": "Selasa, 20\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:55",
+            "ashar": "15:14",
+            "maghrib": "17:53",
+            "isya": "19:03",
+            "date": 8
+        }, {
+            "tanggal": "Rabu, 21\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:55",
+            "ashar": "15:14",
+            "maghrib": "17:53",
+            "isya": "19:02",
+            "date": 9
+        }, {
+            "tanggal": "Kamis, 22\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:55",
+            "ashar": "15:14",
+            "maghrib": "17:52",
+            "isya": "19:02",
+            "date": 10
+        }, {
+            "tanggal": "Jumat, 23\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:55",
+            "ashar": "15:14",
+            "maghrib": "17:52",
+            "isya": "19:02",
+            "date": 11
+        }, {
+            "tanggal": "Sabtu, 24\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:52",
+            "isya": "19:02",
+            "date": 12
+        }, {
+            "tanggal": "Minggu, 25\/04\/2021",
+            "imsak": "04:27",
+            "subuh": "04:37",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:51",
+            "isya": "19:01",
+            "date": 13
+        }, {
+            "tanggal": "Senin, 26\/04\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:51",
+            "isya": "19:01",
+            "date": 14
+        }, {
+            "tanggal": "Selasa, 27\/04\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:51",
+            "isya": "19:01",
+            "date": 15
+        }, {
+            "tanggal": "Rabu, 28\/04\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:50",
+            "isya": "19:01",
+            "date": 16
+        }, {
+            "tanggal": "Kamis, 29\/04\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:54",
+            "ashar": "15:14",
+            "maghrib": "17:50",
+            "isya": "19:01",
+            "date": 17
+        }, {
+            "tanggal": "Jumat, 30\/04\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:14",
+            "maghrib": "17:50",
+            "isya": "19:00",
+            "date": 18
+        }, {
+            "tanggal": "Sabtu, 01\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:14",
+            "maghrib": "17:50",
+            "isya": "19:00",
+            "date": 19
+        }, {
+            "tanggal": "Minggu, 02\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:14",
+            "maghrib": "17:49",
+            "isya": "19:00",
+            "date": 20
+        }, {
+            "tanggal": "Senin, 03\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:14",
+            "maghrib": "17:49",
+            "isya": "19:00",
+            "date": 21
+        }, {
+            "tanggal": "Selasa, 04\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:14",
+            "maghrib": "17:49",
+            "isya": "19:00",
+            "date": 22
+        }, {
+            "tanggal": "Rabu, 05\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:49",
+            "isya": "19:00",
+            "date": 23
+        }, {
+            "tanggal": "Kamis, 06\/05\/2021",
+            "imsak": "04:26",
+            "subuh": "04:36",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:49",
+            "isya": "19:00",
+            "date": 24
+        }, {
+            "tanggal": "Jumat, 07\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "19:00",
+            "date": 25
+        }, {
+            "tanggal": "Sabtu, 08\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "19:00",
+            "date": 26
+        }, {
+            "tanggal": "Minggu, 09\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "18:59",
+            "date": 27
+        }, {
+            "tanggal": "Senin, 10\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:18",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "18:59",
+            "date": 28
+        }, {
+            "tanggal": "Selasa, 11\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:19",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "18:59",
+            "date": 29
+        }, {
+            "tanggal": "Rabu, 12\/05\/2021",
+            "imsak": "04:25",
+            "subuh": "04:35",
+            "terbit": "05:50",
+            "dhuha": "06:19",
+            "dzuhur": "11:53",
+            "ashar": "15:13",
+            "maghrib": "17:48",
+            "isya": "18:59",
+            "date": 30
+        }];
+
+        var today = ramadanDate - 1;
+        var imsak = document.querySelector("#imsak span");
+        var subuh = document.querySelector("#subuh span");
+        var dzuhur = document.querySelector("#dzuhur span");
+        var ashar = document.querySelector("#ashar span");
+        var magrib = document.querySelector("#magrib span");
+        var isya = document.querySelector("#isya span");
+
+        var elements = document.querySelectorAll(".jadwal-2021__content div span")
+
+        var next = document.querySelector("#jadwal-next");
+        var prev = document.querySelector("#jadwal-prev");
+
+        imsak.innerHTML = jadwal[today].imsak;
+        subuh.innerHTML = jadwal[today].subuh;
+        dzuhur.innerHTML = jadwal[today].dzuhur;
+        ashar.innerHTML = jadwal[today].ashar;
+        magrib.innerHTML = jadwal[today].maghrib;
+        isya.innerHTML = jadwal[today].isya;
